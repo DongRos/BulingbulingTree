1a25e3ce7a6907c4bee87b7b749fdc945493053f
DongRos <163140596+DongRos@users.noreply.github.com>
2025-12-19 10:11:09 +0800

    Update UIOverlay.tsx

--- components/UIOverlay.tsx
+++ components/UIOverlay.tsx
@@ -35,7 +35,7 @@ const UIOverlay: React.FC<UIOverlayProps> = ({
       </div>
 
       {/* === 右上角折叠菜单 === */}
-      <div className="absolute top-32 md:top-12 right-12 z-30 flex flex-col items-end">
+      <div className="absolute top-32 md:top-12 right-4 z-30 flex flex-col items-end">
         {/* 展开/收起按钮 - 极简奢华风格 */}
         <button 
           onClick={() => setIsMenuOpen(!isMenuOpen)}
